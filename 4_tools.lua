ffba807123d1bb7e962463ae61aeba08dc409769
CDZGold <mflp55@hotmail.com>
2024-04-08 00:10:47 -0300

    Update 4_tools.lua

--- 4_tools.lua
+++ 4_tools.lua
@@ -144,3 +144,24 @@ UI.DualScrollPanel(storage.manaTrain, function(widget, newParams)
 end)
 
 UI.Separator()
+
+local idz = {13328, 13329, 13330, 13331, 13332, 13333, 13334, 13335, 13336, 13337, 13338, 13339, 13340, 13341, 13342, 13428, 14256, 13429, 13427, 13422, 13430, 13424, 13426, 14255, 13431, 13423, 13421, 13425, 14261, 14263}
+
+macro(300, "Coletar itens do chão", function()
+    local playerPos = g_game.getLocalPlayer():getPosition()
+    local z = playerPos.z 
+    local tiles = g_map.getTiles(posz()) 
+    for _, tile in ipairs(tiles) do 
+        if z ~= playerPos.z then return end 
+        if g_game.getAttackingCreature() == nil then
+            if getDistanceBetween(pos(), tile:getPosition()) <= 8 then
+                local topThing = tile:getTopMoveThing()
+                if topThing and table.find(idz, topThing:getId()) then 
+                    g_game.move(topThing, {x = 65535, y = SlotAmmo, z = 0}, topThing:getCount()) 
+                end
+            end
+        end
+    end
+end)
+
+UI.Separator()
