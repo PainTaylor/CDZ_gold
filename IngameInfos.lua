e96fe5c2525418d6042bbb02648a8559baffa35c
CDZGold <mflp55@hotmail.com>
2024-04-09 10:02:17 -0300

    Update IngameInfos.lua

--- IngameInfos.lua
+++ IngameInfos.lua
@@ -318,22 +318,22 @@ UI.TextEdit(storage.lider or "Blintz", function(widget, newText)
     end)
 
 aviso = macro(100, 'aviso guild', function()
-    for name, _ in pairs(talkedSpecs) do
-        if not getCreatureByName(name) then
-            talkedSpecs[name] = nil
-        end
-    end
-    for _, spec in ipairs(getSpectators()) do
-        if spec ~= player and spec:isPlayer() and not isFriend(spec) then
-            if spec:getEmblem() ~= 1 then
-                local specName = spec:getName()
-                if not talkedSpecs[specName] and (spec:getName() ~= storage.excluido and spec:getName() ~= storage.excluido2 and spec:getName() ~= storage.excluido3 and spec:getName() ~= storage.excluido4) then
-                    talkPrivate(storage.lider, specName .. " - " .. hours .. ':' .. mins .. ':' .. seconds)
-                    talkedSpecs[specName] = true
-                end
-            end
-        end
-    end
+--    for name, _ in pairs(talkedSpecs) do
+--       if not getCreatureByName(name) then
+--            talkedSpecs[name] = nil
+--        end
+--    end
+--    for _, spec in ipairs(getSpectators()) do
+--        if spec ~= player and spec:isPlayer() and not isFriend(spec) then
+--           if spec:getEmblem() ~= 1 then
+--                local specName = spec:getName()
+--                if not talkedSpecs[specName] and (spec:getName() ~= storage.excluido and spec:getName() ~= storage.excluido2 and spec:getName() ~= storage.excluido3 and spec:getName() ~= storage.excluido4) then
+--                    talkPrivate(storage.lider, specName .. " - " .. hours .. ':' .. mins .. ':' .. seconds)
+--                    talkedSpecs[specName] = true
+--               end
+--            end
+--       end
+--    end
 end)
 
 local function add(t, text, color, last)
