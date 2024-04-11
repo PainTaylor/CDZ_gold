35cd0b8a587721e62b8cd72e4dc60a3961c45ae6
CDZGold <mflp55@hotmail.com>
2024-04-11 08:06:10 -0300

    Passing all codes to Github

---
+++ Loader Info DIscord.lua
@@ -0,0 +1,7 @@
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/main/Warming%20Leveling%20DC.lua', function(script)
+    assert(loadstring(script))()
+  end);
+
+modules.corelib.HTTP.get('https://github.com/PainTaylor/CDZ_gold/blob/main/Warming%20Sense%20DC.lua', function(script)
+    assert(loadstring(script))()
+  end);
