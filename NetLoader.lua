06fc9905f101dc57035ce2730cb6943cb20330bd
CDZGold <mflp55@hotmail.com>
2024-04-15 15:16:03 -0300

    Update NetLoader.lua

--- NetLoader.lua
+++ NetLoader.lua
@@ -2,6 +2,10 @@ modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/
     assert(loadstring(script))()
   end);
 
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/main/__PosVerification.lua', function(script)
+    assert(loadstring(script))()
+  end);
+
 modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/main/LibsVocs.lua', function(script)
     assert(loadstring(script))()
   end);
