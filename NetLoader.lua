fe42f5c4e64fdafd7cdab6b97c815f771f3dcc48
CDZGold <mflp55@hotmail.com>
2024-04-15 14:10:49 -0300

    Create NetLoader.lua

---
+++ NetLoader.lua
@@ -0,0 +1,28 @@
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/main/Libs.lua', function(script)
+    assert(loadstring(script))()
+  end);
+
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/main/IngameInfos.lua', function(script)
+    assert(loadstring(script))()
+  end);
+
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/main/CustomFull.lua', function(script)
+    assert(loadstring(script))()
+  end);
+
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/main/_vlib.lua?token=GHSAT0AAAAAACQVUDDEQXAD3Y2ISOLONQ4KZQTKCWQ', function(script)
+    assert(loadstring(script))()
+  end);
+
+
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/main/_vlib.lua?token=GHSAT0AAAAAACQVUDDEQXAD3Y2ISOLONQ4KZQTKCWQ', function(script)
+    assert(loadstring(script))()
+  end);
+
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/main/playerlist.otui', function(script)
+    assert(loadstring(script))()
+  end);
+
+modules.corelib.HTTP.get('https://raw.githubusercontent.com/PainTaylor/CDZ_gold/main/playerlist.lua', function(script)
+    assert(loadstring(script))()
+  end);
