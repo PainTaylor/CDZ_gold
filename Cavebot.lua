731793359c4a164cfff83b391da2c21cd93702eb
CDZGold <mflp55@hotmail.com>
2024-04-17 08:18:45 -0300

    Update Cavebot.lua

--- Cavebot.lua
+++ Cavebot.lua
@@ -1,5 +1,35 @@
 setDefaultTab("Cave")
 
+macro(100, 'Check Armor', function()
+  if storage.timecheck < now then
+  storage.timecheck = now + 60000
+  g_game.look(getInventoryItem(SlotBody))
+end
+end)
+
+UI.TextEdit(storage.mindurability or "80", function(widget, newText)
+storage.mindurability = newText
+end)
+
+Backincave = macro(200, '100%',function()end)
+
+UI.Separator()
+
+-- Cavebot by otclient@otclient.ovh
+-- visit http://bot.otclient.ovh/
+
+setDefaultTab("Cave")
+
+macro(200, 'safe hunt', function()
+if hppercent() < 50 then
+CaveBot.setOff()
+end
+if hppercent() == 100 and manapercent() == 100 then
+CaveBot.setOn()
+end
+end)
+
+
 CaveBot = {} -- global namespace
 CaveBot.Extensions = {}
 importStyle("/cavebot/cavebot.otui")
