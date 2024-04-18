09e65b0e1d0d7634c2f2b9285773b9c8885fae6e
CDZGold <mflp55@hotmail.com>
2024-04-18 13:20:42 -0300

    Update Main.lua

--- Atual/Main.lua
+++ Atual/Main.lua
@@ -1,3 +1,34 @@
+setDefaultTab("Main")
+
+-- allows to test/edit bot lua scripts ingame, you can have multiple scripts like this, just change storage.ingame_lua
+UI.Button("Ingame macro editor", function(newText)
+  UI.MultilineEditorWindow(storage.ingame_macros or "", {title="Macro editor", description="You can add your custom macros (or any other lua code) here"}, function(text)
+    storage.ingame_macros = text
+    reload()
+  end)
+end)
+UI.Button("Ingame hotkey editor", function(newText)
+  UI.MultilineEditorWindow(storage.ingame_hotkeys or "", {title="Hotkeys editor", description="You can add your custom hotkeys/singlehotkeys here"}, function(text)
+    storage.ingame_hotkeys = text
+    reload()
+  end)
+end)
+
+UI.Separator()
+
+for _, scripts in ipairs({storage.ingame_macros, storage.ingame_hotkeys}) do
+  if type(scripts) == "string" and scripts:len() > 3 then
+    local status, result = pcall(function()
+      assert(load(scripts, "ingame_editor"))()
+    end)
+    if not status then 
+      error("Ingame edior error:\n" .. result)
+    end
+  end
+end
+
+UI.Separator()
+
 if not storage.timers then  storage.timers = {  time = 1 } end
 local widgetTC = setupUI([[
 Panel
@@ -1525,36 +1556,6 @@ if x.z ~= y.z then
   end)
 end
 end)
-
-setDefaultTab("Main")
--- allows to test/edit bot lua scripts ingame, you can have multiple scripts like this, just change storage.ingame_lua
-UI.Button("Ingame macro editor", function(newText)
-  UI.MultilineEditorWindow(storage.ingame_macros or "", {title="Macro editor", description="You can add your custom macros (or any other lua code) here"}, function(text)
-    storage.ingame_macros = text
-    reload()
-  end)
-end)
-UI.Button("Ingame hotkey editor", function(newText)
-  UI.MultilineEditorWindow(storage.ingame_hotkeys or "", {title="Hotkeys editor", description="You can add your custom hotkeys/singlehotkeys here"}, function(text)
-    storage.ingame_hotkeys = text
-    reload()
-  end)
-end)
-
-UI.Separator()
-
-for _, scripts in ipairs({storage.ingame_macros, storage.ingame_hotkeys}) do
-  if type(scripts) == "string" and scripts:len() > 3 then
-    local status, result = pcall(function()
-      assert(load(scripts, "ingame_editor"))()
-    end)
-    if not status then 
-      error("Ingame edior error:\n" .. result)
-    end
-  end
-end
-
-UI.Separator()
 
 
 info('Loaded Main')
