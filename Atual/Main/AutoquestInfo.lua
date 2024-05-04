406ac3a50f25bdb63048440bb83bdce543b3df28
CDZGold <mflp55@hotmail.com>
2024-05-04 17:07:47 -0300

    Update AutoquestInfo.lua

--- Atual/Main/AutoquestInfo.lua
+++ Atual/Main/AutoquestInfo.lua
@@ -13,6 +13,15 @@ onTextMessage(function(mode, text)
     if text:find('Daichi') then
       storage.quest = 'daichi'
     end
+    if text:find('Ushio') then
+      storage.quest = 'ushio'
+    end
+    if text:find('Sho') then
+      storage.quest = 'sho'
+    end
+    if text:find('Ichi') then
+      storage.quest = 'ichi'
+    end
   end
 end)
 
@@ -32,7 +41,18 @@ if name == 'Grande Mestre' and text:find('faltam') then
   if text:find('Graad') then
     storage.actualquest = 'graad'
   end
+  if text:find('Ushios') then
+    storage.actualquest = 'ushios'
+  end
+  if text:find('Sho') then
+    storage.actualquest = 'sho'
+  end
+  if text:find('Ichi') then
+    storage.actualquest = 'ichi'
+  end
 end
 end)
 
 info('Loaded Quests')
+
+
