f91c76abe294f0644c4be75732a72a9abbdb077a
Paintaylor Projects <mflp55@hotmail.com>
2024-05-28 15:20:19 -0300

    Update CaveBoster.lua

--- Atual/CaveBoster.lua
+++ Atual/CaveBoster.lua
@@ -2,27 +2,60 @@ setDefaultTab("Cave")
 
 storage.itemxp = now
 macro(200, 'XpBooster', function()
-if storage.itemxp < now then
-use(13226)
-use(13282)
-use(13228)
-use(13420)
-if player:getLevel() >= 300 then
-use(14288)
-end
-end
+	if storage.star5 < now then
+		use(13282)
+	end
+	if storage.star15 < now then
+		use(12228)
+	end
+	if storage.star20 < now then
+		use(13226)
+	end
+	if storage.star25 < now then
+		use(13420)
+	end
+	if player:getLevel() >= 300 and storage.star50 < now then
+		use(14288)
+	end
 end)
+if storage.star5 = nil then
+	storage.star5 = now
+end
+if storage.star15 = nil then
+	storage.star15 = now
+end
+if storage.star20 = nil then
+	storage.star20 = now
+end
+if storage.star25 = nil then
+	storage.star25 = now
+end
+if storage.star50 = nil then
+	storage.star50 = now
+end
 
 onTextMessage(function(mode, text)
 if text:find('Bonus ativado') then
-if player:getLevel() >= 300 then
-totalbonus = '115'
-else
-totalbonus = '65'
-end
-if text:find(totalbonus) then
-storage.itemxp = now + 1800000
-end
+	if text:find('+5%') then
+		storage.star5active = true
+		storage.star5 = now + 1800000
+	end
+	if text:find('+15%') then
+		storage.star15active = true
+		storage.star15 = now + 1800000
+	end
+	if text:find('+20%') then
+		storage.star20active = true
+		storage.star20 = now + 1800000
+	end
+	if text:find('+25%') then
+		storage.star25active = true
+		storage.star25 = now + 1800000
+	end
+	if text:find('+50%') then
+		storage.star50active = true
+		storage.star50 = now + 1800000
+	end
 end
 end)
 
