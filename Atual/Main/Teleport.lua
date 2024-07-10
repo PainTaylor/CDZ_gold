d20aa0a908a812c18ba1ba5a848d4aae09cca441
Paintaylor Projects <mflp55@hotmail.com>
2024-07-10 16:09:49 -0300

    Update Teleport.lua

--- Atual/Main/Teleport.lua
+++ Atual/Main/Teleport.lua
@@ -140,4 +140,126 @@ onKeyDown(function(keys)
     end
 end);
 
-info('Loaded TPs')
+------------------------------------------------------------------
+
+
+
+local ArconteUI = setupUI([[
+MainWindow
+  id: main
+  !text: tr('Minoru Teleport by Kays')
+  size: 230 310
+  scrollable: true
+    
+  ScrollablePanel
+    id: TpList
+    anchors.top: parent.top
+    anchors.left: parent.left
+    size: 190 225
+    vertical-scrollbar: mainScroll
+
+    Button
+      !text: tr('Cerberus Pines')
+      anchors.top: parent.top
+      anchors.left: parent.left
+      width: 165
+
+    Button
+      !text: tr('Butterfly Mountain')
+      anchors.top: prev.bottom
+      anchors.left: parent.left
+      margin-top: 5
+      width: 165
+
+    Button
+      !text: tr('Wolf Covio')
+      anchors.top: prev.bottom
+      anchors.left: parent.left
+      margin-top: 5
+      width: 165
+
+    Button
+      !text: tr('Aries Secret')
+      anchors.top: prev.bottom
+      anchors.left: parent.left
+      margin-top: 5
+      width: 165
+      
+    Button
+      !text: tr('Odrill')
+      anchors.top: prev.bottom
+      anchors.left: parent.left
+      margin-top: 5
+      width: 165
+
+  VerticalScrollBar  
+    id: mainScroll
+    anchors.top: parent.top
+    anchors.bottom: parent.bottom
+    anchors.right: parent.right
+    step: 48
+    pixels-scroll: true
+    
+  Button
+    id: closeButton
+    !text: tr('Close')
+    font: cipsoftFont
+    anchors.right: parent.right
+    anchors.bottom: parent.bottom
+    size: 45 21
+    margin-top: 15
+    margin-right: 15
+
+]], g_ui.getRootWidget());
+ArconteUI:hide();
+
+TpMinoru = {};
+local ArcontePainel = ArconteUI.main;
+local ArconteTPlist = ArconteUI.TpList;
+
+TpMinoru.close = function()
+  ArconteUI:hide()
+  schedule(1000, function()
+      NPC.say('bye');
+end)
+end
+
+TpMinoru.show = function()
+    ArconteUI:show();
+    ArconteUI:raise();
+    ArconteUI:focus();
+end
+
+ArconteUI.closeButton.onClick = function()
+    TpMinoru.close();
+end
+
+TpMinoru.tpToCity = function(city)
+    NPC.say(city);
+    schedule(500, function()
+        NPC.say('yes');
+    end);
+end
+
+
+for i, child in pairs(TpList:getChildren()) do
+    child.onClick = function()
+        TpMinoru.tpToCity(child:getText())
+    end
+end
+
+onTalk(function(name, level, mode, text, channelId, pos)
+  if (name ~= 'Athena Travel') then return; end              
+  if (mode ~= 51) then return; end
+  if (text:find('Para onde gostaria de ir?')) then 
+      TpMinoru.show();
+  else
+      TpMinoru.close();
+  end
+end);
+
+onKeyDown(function(keys)
+    if (keys == 'Escape' and ArconteUI:isVisible())  then
+        TpMinoru.close();
+    end
+end);
