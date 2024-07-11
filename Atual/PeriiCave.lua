ff27ac11da10af57f101e875c4aa201868084386
Paintaylor Projects <mflp55@hotmail.com>
2024-07-10 22:44:34 -0300

    Update PeriiCave.lua

--- Atual/PeriiCave.lua
+++ Atual/PeriiCave.lua
@@ -1,9 +1,9 @@
+setDefaultTab("Cave")
 UI.Label('Numero de Voltas')
+
 UI.TextEdit(storage.voltas or "30", function(widget, newText)
 storage.voltas = newText
 end)
-
-setDefaultTab("Cave")
 
 onKeyDown(function(keys)
   if keys == 'F12' then
