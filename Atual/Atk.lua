bd9058addb25d656cb0ae4e556e7bd1ae3b11916
Paintaylor Projects <mflp55@hotmail.com>
2024-08-29 16:29:28 -0300

    Update Atk.lua

--- Atual/Atk.lua
+++ Atual/Atk.lua
@@ -30,6 +30,48 @@ specialcast = macro(1000, 'Special Cast', function()
   end
 end)
 
+-- Auto Wave Enemy - Turn to Target and say spell
+
+-- START CONFIG
+local max_distance = 5
+local exhausted = 30000 -- milliseconds
+-- END CONFIG
+
+UI.Separator()
+
+-- vBot scripting services: F.Almeida#8019
+-- if you like it, consider making a donation:
+-- https://www.paypal.com/donate/?business=8XSU4KTS2V9PN&no_recurring=0&item_name=OTC+AND+OTS+SCRIPTS&currency_code=USD
+
+-- ATTENTION:
+-- Don't edit below this line unless you know what you're doing.
+-- ATENÇÃO:
+-- Não mexa em nada daqui para baixo, a não ser que saiba o que está fazendo.
+-- ATENCIÓN:
+-- No cambies nada desde aquí, solamente si sabes lo que estás haciendo.
+
+panelName = "autoWave"
+if not storage[panelName] then storage[panelName] = { spell = spell, maxDist = max_distance} end
+
+local config = storage[panelName]
+
+local autoWave = macro(100,"Special Reta", function()
+  local maxDist = tonumber(config.maxDist)
+  local spell = storage.ultimate
+  local enemy = target()
+  if not enemy then return true end
+  local pos = player:getPosition()
+  local cpos = enemy:getPosition()
+  if getDistanceBetween(pos,cpos) > maxDist then return true end
+  local diffx = cpos.x - pos.x
+  local diffy = cpos.y - pos.y
+  if diffx > 0 and diffy == 0 then turn(1) cast(spell) delay(exhausted)
+  elseif diffx < 0 and diffy == 0 then turn(3) cast(spell) delay(exhausted)
+  elseif diffx == 0 and diffy > 0 then turn(2) cast(spell) delay(exhausted)
+  elseif diffx == 0 and diffy < 0 then turn(0) cast(spell) delay(exhausted)
+  end
+end)
+
 macro(100, 'Combo', function()
  if not g_game.isAttacking() then return end
  comboLevel()
