41a48a59f243de5493ab978b9dd885575dc32d61
Paintaylor Projects <mflp55@hotmail.com>
2024-10-23 14:39:01 -0300

    Update Tools.lua

--- Atual/Tools.lua
+++ Atual/Tools.lua
@@ -66,6 +66,31 @@ macro(1, 'Troca de Armas', function()
     end
 end)
 
+UI.Label("Auto Follow Name")
+addTextEdit("followleader", storage.followLeader or "player name", function(widget, text)
+storage.followLeader = text
+end)
+--Code
+local toFollowPos = {}
+local followMacro = macro(20, "Follow", function()
+local target = getCreatureByName(storage.followLeader)
+if target then
+local tpos = target:getPosition()
+toFollowPos[tpos.z] = tpos
+end
+if player:isWalking() then return end
+local p = toFollowPos[posz()]
+if not p then return end
+if autoWalk(p, 20, {ignoreNonPathable=true, precision=1}) then
+delay(100)
+end
+end)
+onCreaturePositionChange(function(creature, oldPos, newPos)
+if creature:getName() == storage.followLeader then
+toFollowPos[newPos.z] = newPos
+end
+end)
+
 
 onContainerOpen(function(container, previousContainer)
   if not container:getName():find('grey bag') then return end
